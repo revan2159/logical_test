0d2628aad8b51a5490831ec729a0022b7f3e9f62
Cahya Revanto <cahya.revanto@students.amikom.ac.id>
2022-09-09 11:50:02 +0700

    Update 2_mysql_query.sql

--- 2_mysql_query.sql
+++ 2_mysql_query.sql
@@ -155,8 +155,16 @@ COMMIT;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 
+
 SELECT tb_mahasiswa.mhs_nama, MAX(tb_mahasiswa_nilai.nilai) AS nilai_max
 FROM tb_mahasiswa_nilai
 INNER JOIN tb_mahasiswa ON tb_mahasiswa_nilai.mhs_id = tb_mahasiswa.mhs_id
 GROUP BY tb_mahasiswa_nilai.mhs_id
 
+SELECT tb_mahasiswa.mhs_nama, tb_mahasiswa_nilai.nilai
+FROM tb_mahasiswa_nilai
+INNER JOIN tb_mahasiswa ON tb_mahasiswa_nilai.mhs_id = tb_mahasiswa.mhs_id
+INNER JOIN tb_matakuliah ON tb_mahasiswa_nilai.mk_id = tb_matakuliah.mk_id
+WHERE tb_matakuliah.mk_kode = 'MK202'
+ORDER BY tb_mahasiswa_nilai.nilai DESC
+LIMIT 1;
